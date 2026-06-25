1b304af01dd6a137b3f4608a034c4c2cc3e69b08
SHIVAM GARG <148767875+garg05shivam@users.noreply.github.com>
2026-06-25 14:31:51 +0530

    basic javascript

---
+++ Javascript/practice.js
@@ -0,0 +1,10 @@
+// console.log("Hello, World!");
+// console.log("This is a practice file for JavaScript.");
+// console.log("Let's learn and practice JavaScript together!");
+
+let a=5;
+var b=6;
+function sayhello(){
+    console.log("Hello, World!");
+}
+console.log(a);
